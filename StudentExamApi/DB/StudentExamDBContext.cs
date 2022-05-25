ab4269526963d62038d564c158d3252e4838f9d8
owlhowl <rms.2003.8@gmail.com>
2022-05-26 05:13:30 +1000

    Add DB-Scaffold

--- StudentExamApi/DB/StudentExamDBContext.cs
+++ StudentExamApi/DB/StudentExamDBContext.cs
@@ -33,6 +33,76 @@ namespace StudentExamApi.DB
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Answer>(entity =>
+            {
+                entity.HasIndex(e => e.QuestionId, "IX_Answers_QuestionId");
+
+                entity.Property(e => e.AnswerText).HasColumnType("ntext");
+
+                entity.Property(e => e.StatusId).HasDefaultValueSql("((1))");
+            });
+
+            modelBuilder.Entity<Exam>(entity =>
+            {
+                entity.HasIndex(e => e.StatusId, "IX_Exams_StatusId");
+
+                entity.HasIndex(e => e.SubjectId, "IX_Exams_SubjectId");
+
+                entity.Property(e => e.Title).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Group>(entity =>
+            {
+                entity.HasIndex(e => e.StatusId, "IX_Groups_StatusId");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Question>(entity =>
+            {
+                entity.HasIndex(e => e.ExamId, "IX_Questions_ExamId");
+
+                entity.Property(e => e.QuestionText).HasColumnType("ntext");
+
+                entity.Property(e => e.StatusId).HasDefaultValueSql("((1))");
+            });
+
+            modelBuilder.Entity<Status>(entity =>
+            {
+                entity.ToTable("Status");
+
+                entity.Property(e => e.Name).HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasIndex(e => e.GroupId, "IX_Students_GroupId");
+
+                entity.HasIndex(e => e.StatusId, "IX_Students_StatusId");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<StudentExam>(entity =>
+            {
+                entity.HasIndex(e => e.ExamId, "IX_StudentExams_ExamId");
+
+                entity.HasIndex(e => e.StudentId, "IX_StudentExams_StudentId");
+
+                entity.Property(e => e.Answers).HasColumnType("ntext");
+
+                entity.Property(e => e.DateTaken).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<Subject>(entity =>
+            {
+                entity.HasIndex(e => e.StatusId, "IX_Subjects_StatusId");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
             OnModelCreatingPartial(modelBuilder);
         }
 
